5b205c570b28f77d418d9416a5be021eb959bf12
Marc Rietz <mwrietz@gmail.com>
2021-01-25 21:08:08 -0600

    initial commit

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
-# performance
+# performance.py 
+
+This script plots investment price performance of a list of ticker symbols for a specified period. 
+
+## Dependencies
+* matplotlib
+* yfinance 
+
+## Example Usage
+* python3 performance.py 
+
---
+++ performance.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+import matplotlib.pyplot as plt
+import yfinance as yf
+
+#symbols = ['^DJI', '^GSPC' ,'^IXIC', 'AAPL', 'ARKG', 'VOO']
+dates = '10y' # 'ytd', '1d', 1y', '10y'
+
+with open('tickersymbols.txt', 'r') as tickers:
+    symbols = tickers.read().split('\n')
+    symbols = list(filter(None, symbols))
+
+print(f'plotting {dates} performance of {symbols}')
+
+dfs = []
+for symbol in symbols:
+    ticker = yf.Ticker(symbol) 
+    data = ticker.history(period=dates)
+    price = data.iloc[0,:]['Close']
+    no_shares = 10000.0/price
+    data['Performance'] = no_shares*data['Close']
+    dfs.append(data)
+
+ax = plt.gca()
+for df in dfs:
+    df.plot(y='Performance', figsize=(20,14), ax=ax)
+ax.legend(symbols)
+ax.set_title(dates)
+ax.grid()
+plt.show()
+
